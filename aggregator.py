f352f715446d689a3f655c942547953d5d5f7456
Chri1629 <christian.uccheddu@gmail.com>
2020-03-12 11:56:03 +0100

    Adds new data

--- aggregator.py
+++ aggregator.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-dati = pd.read_csv("dpc-covid19-ita-province.csv")
+dati = pd.read_csv("dpc-covid19-ita-regioni.csv")
 regioni = pd.read_csv("regioni.csv")
 #print(dati)
 #print(regioni)
 dati_giusti = pd.merge(dati, regioni, left_on = 'denominazione_regione', right_on = 'regione')
+dati_giusti = dati_giusti.drop('denominazione_regione', axis = 1)
 dati_giusti.to_csv('dati.csv')
--- prevision.py
+++ prevision.py
@@ -5,34 +5,34 @@ import numpy as np
 import matplotlib.dates as mdates
 import plotly.express as px
 from scipy.optimize import curve_fit
+from pandas.plotting import register_matplotlib_converters
 
 # Definition of interpolation function
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
-# import the dataset.
+# Import the dataset.
 dati = pd.read_csv("dati.csv")
-dati = dati.drop('denominazione_regione', axis = 1)
 
-milano = dati.loc[dati['denominazione_provincia'] == 'Milano']
-print(milano)
+regione = dati.loc[dati['regione'] == 'Lombardia']
+print(regione)
 
-milano['data'] = pd.to_datetime(milano['data'])
+regione['data'] = pd.to_datetime(regione['data'])
 
-
-# simple exponential regression
-x = np.linspace(0,1,16)
+pd.plotting.register_matplotlib_converters()
+# Simple exponential regression
+x = np.linspace(0,1,len(regione['data']))
 y = func(x, 2.5, 1.3, 0.5)
 
-popt, pcov = curve_fit(func, x, milano['totale_casi'])
+popt, pcov = curve_fit(func, x, regione['totale_casi'])
 print(popt, pcov)
 # Some basics plot
 
 fig, ax = plt.subplots()
 ax.xaxis.set_major_locator(mdates.MonthLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d'))
-plt.plot(milano['data'], milano['totale_casi'], 'ko', label="Original Data")
-plt.plot(milano['data'], func(x, *popt), 'r-', label="Fitted Curve")
+plt.plot(regione['data'], regione['totale_casi'], 'ko', label="Original Data")
+plt.plot(regione['data'], func(x, *popt), 'r-', label="Fitted Curve")
 plt.legend()
 plt.show()
 '''
