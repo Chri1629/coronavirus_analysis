4ee7727f1ea1b30d15e82dce35a9a608974ac144
Chri1629 <christian.uccheddu@gmail.com>
2020-03-12 12:07:32 +0100

    Transform to functions

--- prevision.py
+++ prevision.py
@@ -3,13 +3,22 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
 import matplotlib.dates as mdates
-import plotly.express as px
 from scipy.optimize import curve_fit
 from pandas.plotting import register_matplotlib_converters
 
 # Definition of interpolation function
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
+
+def time_plot(time, data): 
+    fig, ax = plt.subplots()
+    plt.plot(time, data, 'ko', label="Original Data")
+    plt.plot(time, func(x, *popt), 'r-', label="Fitted Curve")
+    hfmt = mdates.DateFormatter('%d-%m')
+    ax.xaxis.set_major_formatter(hfmt)
+    plt.xticks(rotation = 90, fontsize=8)
+    plt.legend()
+    plt.show()
 
 # Import the dataset.
 dati = pd.read_csv("dati.csv")
@@ -25,18 +34,11 @@ x = np.linspace(0,1,len(regione['data']))
 y = func(x, 2.5, 1.3, 0.5)
 
 popt, pcov = curve_fit(func, x, regione['totale_casi'])
-print(popt, pcov)
+print('--------------------------------------------------------------------------')
+print('I parametri stimati sono: ',popt, pcov)
+
+print('--------------------------------------------------------------------------')
 # Some basics plot
 
-fig, ax = plt.subplots()
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d'))
-plt.plot(regione['data'], regione['totale_casi'], 'ko', label="Original Data")
-plt.plot(regione['data'], func(x, *popt), 'r-', label="Fitted Curve")
-plt.legend()
-plt.show()
-'''
-fig = px.line(milano, x='data', y='totale_casi', var_name = 'Reale')
-fig.add_scatter(x=milano['data'], y=func(x, *popt), var_name = 'Fittato')
-fig.show()
-'''
+time_plot(regione['data'], regione['totale_casi'] )
+
