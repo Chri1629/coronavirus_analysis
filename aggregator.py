775f9e415ead853fc89d76dcf646cd52743fc90d
Chri1629 <christian.uccheddu@gmail.com>
2020-03-21 22:15:08 +0100

    Dataset at 21-03

--- aggregator.py
+++ aggregator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 dati = pd.read_csv("dpc-covid19-ita-regioni.csv")
 regioni = pd.read_csv("regioni.csv")
 dati_giusti = pd.merge(dati, regioni, left_on = 'denominazione_regione', right_on = 'regione')
